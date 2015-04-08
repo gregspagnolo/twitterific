e28e002a0487d590c4133b0a3b08cde9c57188e8
gregspagnolo <dwdrummer@gmail.com>
2015-04-07 22:50:46 -0500

    fix something else

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'thawing-ravine-9518.herokuapp.com'
-  config.action_mailer.default_url_options = { host: 'example.com' }
+  config.action_mailer.default_url_options = { host: 'thawing-ravine-9518.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
