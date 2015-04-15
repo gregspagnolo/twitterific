1f855fe4e446d783e1d6da28fec7b52804fbc506
gregspagnolo <dwdrummer@gmail.com>
2015-04-14 22:32:51 -0500

    fix stuff

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,12 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+  	config.fog_provider = 'fog-aws'
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJMSH5ZB4ZG7BMBUQ'],
-      :aws_secret_access_key => ENV['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']
+      provider:             => 'AWS',
+      aws_access_key_id:     => ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['twittergms']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
